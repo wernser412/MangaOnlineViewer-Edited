300ae2b659f16040b2fa383518610beb50a4b372
wernser412 <53667212+wernser412@users.noreply.github.com>
2023-04-27 17:15:10 -0500

    Update Manga OnlineViewer Edited.user.js

--- Manga OnlineViewer Edited.user.js
+++ Manga OnlineViewer Edited.user.js
@@ -64,7 +64,7 @@
 // @include       /https?:\/\/(www.)?shimadascans.com\/.+(series|chapter).+/
 // @include       /https?:\/\/(www.)?tapas.io\/episode\/.+/
 // @include       /https?:\/\/(www.)?(tenmanga|gardenmanage).com\/(chapter|statuses)\/.+/
-// @include       /https?:\/\/(www.)?(almtechnews|animalcanine|animalslegacy|animalsside|animation2you|animationforyou|anisurion|anitirion|anitoc|cook2love|cooker2love|cookermania|cookernice|cookerready|dariusmotor|enginepassion|fanaticmanga|feelthecook|followmanga|gamesnk|gamesxo|infogames2you|infopetworld|keepfooding|lectortmo|mangalong|mistermanga|motorbakery|motornk|motoroilblood|motorpi|mygamesinfo|mynewsrecipes|myotakuinfo|otakunice|otakuworldgames|otakworld|paleomotor|panicmanga|recetchef|recipesaniki|recipescoaching|recipesdo|recipesist|recipesnk|releasingcars|sucrecipes|tmofans|vgmotor|vsrecipes|worldmangas|wtechnews).com\/(viewer|news)\/.+\/(paginated|cascade)/
+// @include       /https?:\/\/(www.)?(almtechnews|animalcanine|animalslegacy|animalsside|animation2you|animationforyou|anisurion|anitirion|anitoc|checkingcars|cook2love|cooker2love|cookermania|cookernice|cookerready|dariusmotor|enginepassion|fanaticmanga|feelthecook|followmanga|gamesnk|gamesxo|infogames2you|infopetworld|keepfooding|lectortmo|mangalong|mistermanga|motorbakery|motornk|motoroilblood|motorpi|mygamesinfo|mynewsrecipes|myotakuinfo|otakunice|otakuworldgames|otakworld|paleomotor|panicmanga|recetchef|recipesaniki|recipescoaching|recipesdo|recipesist|recipesnk|releasingcars|sucrecipes|tmofans|vgmotor|vsrecipes|worldmangas|wtechnews).com\/(viewer|news)\/.+\/(paginated|cascade)/
 // @include       /https?:\/\/(www.)?unionleitor.top\/leitor\/.+\/.+/
 // @include       /https?:\/\/(www.)?webnovel.com\/comic\/.+/
 // @include       /https?:\/\/(www.)?webtoons.com\/.+viewer.+/
@@ -1181,6 +1181,7 @@
     "cookermania",
     "cookernice",
     "cookerready",
+    "checkingcars",
     "dariusmotor",
     "enginepassion",
     "fanaticmanga",
